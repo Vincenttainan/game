30a363998f8d110e2223df46157310e3bd83f24b
Vincenttainan <vincent.tainan888@gmail.com>
2019-09-03 21:34:36 +0800

    first

---
+++ game.py
@@ -0,0 +1,275 @@
+a=input('What is your name?')
+print('Hi ',a,' this is your blood volume!')
+print('❤❤❤❤')
+print('And this is you!')
+print('@')
+print('This is your enemy!')
+print('$')
+print('Find your enemy and destroy them!')
+print('Ho!By the why!Do not ever touch the * or X!')
+print('❤❤❤❤  score:0')
+print('* * * * * * *')
+print('*       * * *')
+print('*   * $     *')
+print('* $ * * *   *')
+print('* * * * *   *')
+print('* @     $   *')
+print('* * * * * * *')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('❤❤❤  score:0')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * *   *')
+    print('* * * * *   *')
+    print('* X @   $   *')
+    print('* * * * * * *')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('❤❤❤❤  score:0')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * *   *')
+    print('* * * * *   *')
+    print('* X X @ $   *')
+    print('* * * * * * *')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * *   *')
+    print('* * * * *   *')
+    print('* X X X @   *')
+    print('* * * * * * *')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * *   *')
+    print('* * * * *   *')
+    print('* X X X X @ *')
+    print('* * * * * * *')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * *   *')
+    print('* * * * * @ *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $     *')
+    print('* $ * * * @ *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $   @ *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('❤❤❤  score:1')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * $ @ X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('❤❤   score:2')
+    print('* * * * * * *')
+    print('*       * * *')
+    print('*   * @ X X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('❤❤   score:2')
+    print('* * * * * * *')
+    print('*     @ * * *')
+    print('*   * X X X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('❤❤   score:2')
+    print('* * * * * * *')
+    print('*   @ X * * *')
+    print('*   * X X X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('❤❤   score:2')
+    print('* * * * * * *')
+    print('* @ X X * * *')
+    print('*   * X X X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='s':
+    print('You died')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('❤❤   score:2')
+    print('* * * * * * *')
+    print('* X X X * * *')
+    print('* @ * X X X *')
+    print('* $ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+if b=='d':
+    print('You died')
+b=input('Go where?(w/a/s/d)')
+if b=='w':
+    print('You died')
+if b=='a':
+    print('You died')
+if b=='s':
+    print('❤     score:3')
+    print('* * * * * * *')
+    print('* X X X * * *')
+    print('* X * X X X *')
+    print('* @ * * * X *')
+    print('* * * * * X *')
+    print('* X X X X X *')
+    print('* * * * * * *')
+    print('Great!You destroy all of your enemies!')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+    print('d(^_^o)d(^_^o)d(^_^o)d(^_^o)d(^_^o)')
+
+if b=='d':
+    print('You died')
+print('End~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+print('Writer~~~~Vincent Yao~~~~~~~~~~~~~~')
